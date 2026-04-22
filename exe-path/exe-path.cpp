2e833d291fe913d01131fc281026b19b4667847b
high on tantor <josephvenable@verizon.net>
2026-04-22 04:32:51 -0400

    Update exe-path.cpp

--- exe-path/exe-path.cpp
+++ exe-path/exe-path.cpp
@@ -84,7 +84,7 @@
 std::string get_executable_path(int process_id) {
   std::string path;
   if (process_id < -1) {
-    errno = EACCES;
+    errno = ESRCH;
     return path;
   }
   #if defined(_WIN32)

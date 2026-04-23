ff0d28a31cd6f3749bfbba0fe2d478f5d0ee57a8
high on tantor <josephvenable@verizon.net>
2026-04-22 21:19:07 -0400

    Update pidpath.sh

--- pidpath.sh
+++ pidpath.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 cd "${0%/*}"
 if [ $(uname) = "Darwin" ]; then
-  clang++ pidpath/pidpath.cpp pidpath.cpp -o a.out -std=c++17 -Wall -arch arm64 -arch x86_64; ./a.out;
+  clang++ pidpath/pidpath.cpp pidpath.cpp -o pidpath -std=c++17 -Wall -arch arm64 -arch x86_64; ./pidpath;
 elif [ $(uname) = "Linux" ]; then
   if [ -f "/bin/g++" ]; then
-    g++ pidpath/pidpath.cpp pidpath.cpp -o a.out -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./a.out;
+    g++ pidpath/pidpath.cpp pidpath.cpp -o pidpath -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./pidpath;
   else
-    clang++ pidpath/pidpath.cpp pidpath.cpp -o a.out -std=c++17 -Wall; ./a.out;
+    clang++ pidpath/pidpath.cpp pidpath.cpp -o pidpath -std=c++17 -Wall; ./pidpath;
   fi
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ pidpath/pidpath.cpp pidpath.cpp -o a.out -std=c++17 -Wall -static; ./a.out;
+  clang++ pidpath/pidpath.cpp pidpath.cpp -o pidpath -std=c++17 -Wall -static; ./pidpath;
 elif [ $(uname) = "DragonFly" ]; then
-  g++ pidpath/pidpath.cpp pidpath.cpp -o a.out -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./a.out;
+  g++ pidpath/pidpath.cpp pidpath.cpp -o pidpath -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./pidpath;
 elif [ $(uname) = "NetBSD" ]; then
-  g++ pidpath/pidpath.cpp pidpath.cpp -o a.out -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./a.out;
+  g++ pidpath/pidpath.cpp pidpath.cpp -o pidpath -std=c++17 -Wall -static-libgcc -static-libstdc++ -static; ./pidpath;
 elif [ $(uname) = "OpenBSD" ]; then
-  clang++ pidpath/pidpath.cpp pidpath.cpp -o a.out -std=c++17 -Wall -lkvm -static; ./a.out;
+  clang++ pidpath/pidpath.cpp pidpath.cpp -o pidpath -std=c++17 -Wall -lkvm -static; ./pidpath;
 elif [ $(uname) = "SunOS" ]; then
-  g++ pidpath/pidpath.cpp pidpath.cpp -o a.out -std=c++17 -Wall -static-libgcc -lproc; ./a.out;
+  g++ pidpath/pidpath.cpp pidpath.cpp -o pidpath -std=c++17 -Wall -static-libgcc -lproc; ./pidpath;
 else
-  g++ pidpath/pidpath.cpp pidpath.cpp -o a.out.exe -std=c++17 -Wall -static-libgcc -static-libstdc++ -static -Wl,--subsystem,console; ./a.out.exe;
+  g++ pidpath/pidpath.cpp pidpath.cpp -o pidpath.exe -std=c++17 -Wall -static-libgcc -static-libstdc++ -static -Wl,--subsystem,console; ./pidpath.exe;
 fi

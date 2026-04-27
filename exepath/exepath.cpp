eb8d7dad6abf3004a07474a073c2f2e2077bcbaa
high on tantor <josephvenable@verizon.net>
2026-04-26 22:04:11 -0400

    Update exepath.cpp

--- exepath/exepath.cpp
+++ exepath/exepath.cpp
@@ -65,7 +65,6 @@
 #elif defined(__OpenBSD__)
 #include <vector>
 #include <sstream>
-#include <cerrno>
 #include <cstddef>
 #include <climits>
 #include <cstdlib>
@@ -275,9 +274,6 @@ std::string get_executable_path() {
         goto fallback;
       }
     }
-  }
-  if (!path.empty()) {
-    errno = 0;
   }
   #elif defined(__sun)
   char exe[PATH_MAX];
